4defa9e1e86ff21d4684136da1cf81e7484eb8f1
Christianngono <christianngonoabanda384@gmail.com>
2024-03-25 09:06:38 +0100

    main.ts

---
+++ main.ts
@@ -0,0 +1,16 @@
+class character {
+    constructor(
+        protected name: string,
+        protected attack: number,
+        protected defense: number,
+        protected speed: number,
+        protected maxHp: number,
+        protected currentHp: number,
+    ) {}
+
+    // methode pour attacquer un autre personnage
+    attackTarget(target: character) {
+        
+    }
+
+}
