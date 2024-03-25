4a62da1ded8c796baa319dc2646203c237a1a077
Christianngono <christianngonoabanda384@gmail.com>
2024-03-25 09:16:24 +0100

    Ginjrvjrnt

--- main.ts
+++ main.ts
@@ -4,13 +4,26 @@ class character {
         protected attack: number,
         protected defense: number,
         protected speed: number,
-        protected maxHp: number,
-        protected currentHp: number,
+        protected maxHP: number,
+        protected currentHP: number,
     ) {}
 
     // methode pour attacquer un autre personnage
     attackTarget(target: character) {
-        
+        const damage = this.attack - target.defense;
+        target.takeDamage(damage);
     }
+
+    // methode pour prendre des dégats
+    takeDamage(damage: number) {
+        this.currentHP -= damage;
+        if (this.currentHP <= 0) {
+            this.currentHP = 0;
+            this.die();
+            return;
+        }
+    }
+    
+
 
 }
