e350c963ffcc71adf792623a80c6ccb641688d33
Christianngono <christianngonoabanda384@gmail.com>
2024-03-25 11:11:39 +0100

    rareedbufe

---
+++ menu.ts
@@ -0,0 +1,32 @@
+class Menu {
+    constructor(private question: string, private options: string[]) {}
+
+    // methode pour afficher le menu
+    display() {
+        console.log(this.question);
+        this.options.forEach((option, index)=> {
+            console.log(`${index + 1}. ${option}`);
+        });    
+    }
+    // methode pour selectionner une option
+    selectOption(option: number): string {
+        return this.options[option - 1];
+    }
+    // methode pour savoir si l'utilisateur a sélectionné une option
+    isOptionSelected(option: number): boolean {
+        return option > 0 && option <= this.options.length;
+    }
+
+    ask(): string {
+        this.display();
+        const choice = prompt("Choisissez une option: ");
+
+        if (!choice || isNaN(parseInt(choice)) || parseInt(choice) < 1 || parseInt(choice) > this.options.length) {
+            console.log("Choix invalide ! Veuillez sélectionner à nouveau.");
+            return this.ask();
+        }
+        return this.options[parseInt(choice) - 1];
+    }
+}
+
+export {Menu, character};
