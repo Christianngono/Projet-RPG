961b87f6bd27c4a7f8c3525d5f3178991fa383ba
Christianngono <christianngonoabanda384@gmail.com>
2024-04-02 19:11:26 +0200

    To review character.ts

--- character.ts
+++ character.ts
@@ -1,5 +1,5 @@
 import { Menu } from "./menu.ts";
-const menu = new Menu("Selectionnez une option :", ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Option 6"]);
+const menu = new Menu("Selectionnez une option :", ["Option 1", "Option 2"]);
 menu.display();
 
 import {Fight} from "./fight.ts";
@@ -14,9 +14,9 @@ class Character {
         protected name: string,
         protected attack: number,
         protected defense: number,
-        protected speed: number,
+        public speed: number,
         protected maxHP: number,
-        protected currentHP: number,
+        public currentHP: number,
     ) {}
 
     // methode pour attacquer un autre personnage
@@ -30,11 +30,14 @@ class Character {
 
     // methode pour prendre des dégats
     takeDamage(attack: number) {
-        const damage = Math.max(this.attack - this.defense);
-        attack += damage;
-        this.takeDamage(damage);
-
+        this.currentHP -= attack;
+        if (this.currentHP < 0) {
+            this.currentHP = 0;
+        }
+        console.log(`${this.name} prend ${this.attack} de dégats`);
+        console.log(`${this.name} reste ${this.currentHP} de vie`);
     }
+
 
     // methode pour restaurer les points de vie
     restoreHp(currentHP: number) {
@@ -51,78 +54,9 @@ class Character {
     }
 }
 
-
-
-class Mage extends Character {
-    constructor(name: string) {
-        super(name, 67, 8, 15, 80, 80);
-    }
-
-    // methode pour l'atteque magique
-    magicAttack(name: Character) {
-        // effectuer les domages magiques sans tenir compte de la défense de l'ennemi
-        const magicDamage = Math.max(0, this.attack - this.defense);
-        name.takeDamage(magicDamage);
-    }
-}
-
-
-class Paladin extends Character {
-    constructor(name: string) {
-        super(name, 55, 65, 12, 90, 90);
-    }
-    // methode pour effectuer une attaque sainte sur un autre personnage
-    holyAttack(name: Character) {
-        console.log(`${this.name} lance une attaque sainte sur ${name}`);
-            const damage = Math.max(0, (this.attack - this.defense) * 0.4);
-            name.takeDamage(damage);
-
-        
-    }
-    // methode pour effectuer une attaque de groupe 
-    groupAttack(name: Character) {
-        console.log("attaque de groupe");
-        const damage = Math.max(0, (this.attack - this.defense) * 0.3);
-        name.takeDamage(damage);   
-    }
-}
-
-
-class Barbare extends Character {
-    constructor(name: string) {
-        super(name, 70, 15, 10, 100, 100);
-    }
-    // methode pour effectuer une attaque berserk sur un autre personnage
-    berserkAttack(name: Character) {
-        console.log(`${this.name} lance une attaque berserk sur ${name}`);
-        const damge = Math.max(0, (this.attack - this.defense) * 1.3);
-        name.takeDamage(damge);
-        const selfDamage = Math.floor(this.currentHP * 0.2);
-        this.takeDamage(selfDamage);
-    }
-}
-
-class Priest extends Character {
-    constructor(name: string) {
-        super(name, 71, 30, 10, 80, 80);
-    }
-    // methode pour prendre soin d'un allié ou de soit même en restaurant 20 pourcent de sa vie
-    heal(name: Character) {
-        console.log(`${this.name} lance un soin sur ${name}`);
-        const healedAmount = Math.floor(this.currentHP * 0.25);
-        name.restoreHp(healedAmount);
-    }   
-}    
 // Création des équipes
-const team1: Character[] = [
-    new Paladin("Arthur"),
-    new Barbare("Kratos"),
-    new Priest("Merlin"),
-];
-
-const team2: Character[] = [
-    new Mage("Gandalf"),
-];
+const team1: Character[] = [];
+const team2: Character[] = [];
 
 const fight = new Fight(team1, team2);
 
