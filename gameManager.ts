89ac1792748d68a8fad24a077629c34451fb717e
Christianngono <christianngonoabanda384@gmail.com>
2024-04-14 13:50:22 +0200

    Ajout de gameManager.ts

--- gameManager.ts
+++ gameManager.ts
@@ -1,8 +1,34 @@
+import {Character} from "./character.ts";
+import {DungeonRoom} from "./dungeons.ts";
+import {Enemy} from "./enemies.ts";
 class GameManager {
+    constructor(public dungeonRooms: DungeonRoom[], public characters: Character[], public enemies: Enemy[]) {}
     startGame() {
-        // implementer la logique de demarrage de jeu
-        console.log("jeu de combat");
+        // Implement game start logic
+        console.log("Combat game");
+
+        this.exploreDungeon();
+    
+
     }
+
+    // Method to explore the dungeon
+    exploreDungeon() {
+        console.log("You are in a dungeon!");
+        this.dungeonRooms.forEach((room) => {
+            if (this.characters.length === 0) {
+                console.log("You have lost. Game Over!");
+                return;
+            }
+            if (this.enemies.length === 0) {
+                console.log("You have won. Game Over!");
+                return;
+            }
+            room.interact(this.characters);
+        });
+        console.log("You have explored all the dungeon rooms!");
+        console.log("End of the game.");
+    }       
 }
 
 export {GameManager};
