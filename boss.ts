c3763cf0c4666c067304147e46633751907f62cc
Christianngono <christianngonoabanda384@gmail.com>
2024-04-14 23:33:41 +0200

    To add this new version

--- boss.ts
+++ boss.ts
@@ -11,7 +11,7 @@ class Boss extends Character {
         if (rand === 0.4) {
             const damage = Math.max(0, (this.attack - this.defense) * 0.4);
             names.forEach((name) => {
-                name.takeDamage(damage, 0);
+                name.takeDamage(damage);
                 console.log(`${this.names} lance une attaque simple sur ${names.map((name) => `${name.names}`).join(", ")}`);
             });  
         }
@@ -22,7 +22,7 @@ class Boss extends Character {
         if (rand === 0.7) {
             const damage = Math.max(0, (this.attack - this.defense) * 0.7);
             names.forEach((name) => {
-                name.takeDamage(damage, 0);
+                name.takeDamage(damage);
                 console.log(`${this.names} lance une attaque simple sur ${names.map((name) => `${name.names}`).join(", ")}`);
             });
         }
@@ -33,7 +33,7 @@ class Boss extends Character {
         if (rand === 0.3) {
             const damage = Math.max(0, (this.attack - this.defense) * 0.3);
             names.forEach((name) => {
-                name.takeDamage(damage, 0);
+                name.takeDamage(damage);
                 console.log(`${this.names} lance une attaque simple sur ${names.map((name) => `${name.names}`).join(", ")}`);
             });
         }
