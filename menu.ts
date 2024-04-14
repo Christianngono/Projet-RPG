7555f87811e19eb4215c30bf2bc9f63153a25e7a
Christianngono <christianngonoabanda384@gmail.com>
2024-04-14 18:42:12 +0200

    Apport de menu.ts

--- menu.ts
+++ menu.ts
@@ -8,6 +8,7 @@ class Menu {
             console.log(`${index + 1}. ${option}`);
         });    
     }
+
     // methode pour selectionner une option
     selectOption(option: number): string {
         if(this.isOptionSelected(option)) {
@@ -29,7 +30,25 @@ class Menu {
             return this.ask();
         }
         return this.options[parseInt(choice) - 1];
-    }    
+    }
+
+    // Méthode pour demander à l'utilisateur de choisir un outil
+    askForObject(): string {
+        const objectsOptions = ["Potion", "StarFragment", "Demi-étoile", "Ether"];
+        console.log("Options d'outils disponibles :");
+        objectsOptions.forEach((option, index) => {
+            console.log(`${index + 1}. ${option}`);
+        });
+
+        const choice = prompt("Choisissez un outil : ");
+
+        if (!choice || isNaN(parseInt(choice)) || parseInt(choice) < 1 || parseInt(choice) > 4) {
+            console.log("Choix invalide ! Veuillez sélectionner à nouveau.");
+            return this.askForObject();
+        }
+        return objectsOptions[parseInt(choice) - 1];
+        
+    }
 }
 
 export {Menu};
