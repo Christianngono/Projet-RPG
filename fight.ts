96f5432c2a78ad44d6ffe563b58f4db083567e18
Christianngono <christianngonoabanda384@gmail.com>
2024-04-14 13:39:04 +0200

    Ajout de fight.ts

--- fight.ts
+++ fight.ts
@@ -1,74 +1,79 @@
 import {Character} from './character.ts';
 
+
 class Fight {
-    constructor(public team1: Character[], public team2: Character[]) {
-        this.team1 = team1;
-        this.team2 = team2;
-    }
-
+    constructor(public characters: Character[], public enemies: Character[]) {}
     
-    // Metode pour determiner l'ordre des tours en fonction de la vitesse de chaque personnage
-    determineTurnOrder(): Character[] {
-        const allCharacters: Character[] = this.team1.concat(this.team2);
-        return allCharacters.sort((a, b) => a.speed - b.speed);
-    }
-
-    // Methode pour verifier si tous les personnages d'une equipe sont morts ou vivants
-    isTeamDead(team: Character[]): boolean {
-        return team.every(character => character.currentHP <= 0);    
-    }
-
-    // Methode pour stimuler un tour de combat
-    stimulateTurn(character: Character) {
+    // Méthode pour commencer le combat
+    start() {
+        console.log("Combat commencé !");
+        while (this.characters.length > 0 && this.enemies.length > 0) {
+            this.charactersAttack();
+            if (this.enemies.length > 0) {
+                this.enemiesAttack();
+            }
+            const characters: Character[] = this.determineOrder();
+            let index = 0;
+            // Ajout d'un compteur de tours
+            let rounds = 0;
+    
+            while (!this.checkAllDead(this.characters) && !this.checkAllDead(this.enemies) && rounds < 4) {
+                this.stimulateTurn(characters[index]);
+                index =(index + 1) % characters.length;
+                rounds++;
+            }
+    
+            if (this.checkAllDead(this.characters)) {
+                console.log("la team1  perdu, la team2 gagné !");
+            } else if (!this.checkAllDead(this.enemies)) {
+                console.log("la team2 perdu, la team1 gagné !");
+            }
+            console.log("fin de combat");
+            return this.checkAllDead(this.characters) || this.checkAllDead(this.enemies);
         
-        // Sélectionner l'équipe cible
-        const targetTeam = character === this.team1[0] ? this.team2 : this.team1;
-
-        // Pour chaque personnage de l'équipe cible, subir l'attaque de l'attaquant
-        targetTeam.forEach((targetCharacter) => {
-            // Vérifier si la cible est vivante et si ce n'est pas le même personnage que l'attaquant
-            if (targetCharacter.currentHP > 0 && targetCharacter !== character) {
-                console.log(`${targetCharacter.names} est vivant !`);
-                console.log(targetCharacter.currentHP);
-                console.log(targetCharacter.attack);
-                console.log(targetCharacter.defense);
-                console.log(targetCharacter.speed);
-                return;
-
-
-            } else {
-                console.log(`${targetCharacter.names} est déjà mort !`);
-                console.log(targetCharacter.currentHP);
-                console.log(targetCharacter.attack);
-                console.log(targetCharacter.defense);
-                console.log(targetCharacter.speed);
-                return;
+        }
+        console.log("Combat terminé !");
+    }
+    charactersAttack() {
+        this.characters .forEach((character) => {
+            if (this.enemies.length > 0) {
+                character.attackTarget(this.enemies);
+                this.enemies = this.enemies.filter((enemy) => enemy.currentHP > 0);
+            }
+        });
+    }
+    enemiesAttack() {
+        this.enemies.forEach((enemy) => {
+            if (this.characters.length > 0) {
+                enemy.attackTarget(this.characters);
+                this.characters = this.characters.filter((character) => character.currentHP > 0);
             }
         });
     }
 
-    // Methode pour commencer un combat
-    start() {
-        console.log("debut de combat !");
-        const characters: Character[] = this.determineTurnOrder();
-        let index = 0;
-        // Ajout d'un compteur de tours
-        let rounds = 0;
-
-        while (!this.isTeamDead(this.team1) && !this.isTeamDead(this.team2) && rounds < 8) {
-            this.stimulateTurn(characters[index]);
-            index =(index + 1) % characters.length;
-            rounds++;
-        }
-
-        if (!this.isTeamDead(this.team1)) {
-            console.log("la team1 perdu, la team2 gagné !");
-        } else if (!this.isTeamDead(this.team2)) {
-            console.log("la team2 perdu, la team1 gagné !");
-        }
-        console.log("fin de combat");
-        return this.isTeamDead(this.team1) || this.isTeamDead(this.team2);
-    }   
+    // Déterminer l'ordre des tours en fonction de la vitesse de chaque personnage
+    determineOrder(): Character[] {
+        const allCharacters: Character[] = this.characters.concat(this.enemies);
+        return allCharacters.sort((a, b) => a.speed - b.speed).reverse();
+    }
+    // Vérifier si tous les personnages d'une equipe sont morts ou vivants
+    checkAllDead(team: Character[]): boolean {
+        return team.every((character) => character.currentHP <= 0);
+    }
+    // Stimuler un tour de combat
+    stimulateTurn(character: Character) {
+        // Sélectionner une équipe cible
+        const target = this.determineTarget(character);
+        // Attaquer la cible
+        character.attackTarget(target);
+    }
+    // Sélectionner une équipe cible
+    determineTarget(character: Character): Character[] {
+        const targetTeam = character === this.characters[0] ? this.enemies : this.characters;
+        const aliveTargets = targetTeam.filter(target => target.currentHP > 0);
+        return aliveTargets.length > 0 ? [aliveTargets[0]] : [];
+    }
 }
 
 export {Fight};
+
