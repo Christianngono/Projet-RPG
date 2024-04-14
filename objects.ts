92a72e9425f463a455ae1d13d12666c3315e1673
Christianngono <christianngonoabanda384@gmail.com>
2024-04-14 18:36:28 +0200

    Mise de objects.ts

--- objects.ts
+++ objects.ts
@@ -1,60 +1,18 @@
-import { Outil } from "./outil.ts";
-
-class Objects extends Outil {
-    // Méthode pour utiliser l'objet
-    use(target: string) {
-        console.log("Vous pouvez utiliser l'objet");
-        // Méthode pour utiliser un objet
-        switch (name) {
-            case "Potion":
-                this.usePotion(target);
-                break;
-
-            case "StarFragment":
-                this.useStarFragment(target);
-                break;
-
-            case "Demi-d'étoile":
-                this.useHalfStar(target);
-                break;
-
-            case "Ether":
-                this.useEther(target);
-                break;
-
-            default:
-                console.log("Vous ne pouvez pas utiliser cet objet");
-                break;    
+class Objects {
+    static Steal(voleur: {getPotion(): string; getFragment(): string; getEther(): string; getHalfStar(): string}): string {
+        const rand = Math.random() * 100;
+        if (rand < 40) {
+            return "rien";
+        } else if (rand  < 70) {
+            return voleur.getPotion();;
+        } else if (rand < 85) {
+            return voleur.getFragment() ;
+        } else if (rand < 95) {
+            return voleur.getEther();
+        } else {
+            return voleur.getHalfStar();
         }
-
     }
-    // Méthode pour utiliser une potion
-    private usePotion(target: any){
-        const restoredHP = Math.floor(target.maxHP * 0.5);
-        target.restoreHP(restoredHP);
-        console.log(`${target.names} utilise une potion et récupère ${restoredHP} points de vie.`);
-    }
-
-    // Méthode pour utiliser un morceau d'étoile
-    private useStarFragment(target: any) {
-        const restoredHP = Math.floor(target.maxHP * 0.2);
-        target.restoreHP(restoredHP);
-        console.log(`${target.names} utilise un morceau d'étoile et récupère ${restoredHP} points de vie.`);
-    }
-
-    // Méthode pour utiliser une demi-étoile
-    private useHalfStar(target: any) {
-        target.restoreHP(target.maxHP);
-        console.log(`${target.names} utilise une demi-étoile et récupère tous ses points de vie.`);
-    }
-
-    // Méthode pour utiliser un éther
-    private useEther(target: any) {
-        const restoredMP = Math.floor(target.maxMP * 0.3);
-        target.restoreMP(restoredMP);
-        console.log(`${target.names} utilise un éther et récupère ${restoredMP} points de magie.`);
-    }
-
 }
 
 export {Objects};
